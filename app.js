dda852c94313b0af6472a8f0193f608115447bc9
Maziar Sojoudian <maziar@sojoudian.net>
2024-08-09 13:43:26 -0400

    add dockerfile

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 const interval = process.env.INTERVAL || 'default interval';
 
 const requestHandler = (req, res) => {
